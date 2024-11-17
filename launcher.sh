e8fd111a1b27e8ca89e795c7432502a5161974d7
Adrian Pfleiderer <github@adrian-pfleiderer.de>
2024-11-17 22:40:54 +0100

    Update launcher.sh
    - fix folder name

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ $PLAYER_EVENT = "playing" ] || [ $PLAYER_EVENT = "stopped" ] || [ $PLAYER_EVENT = "paused" ]; then
-/usr/bin/Pixoo_Album_Art/env/bin/source activate
-python3 /usr/bin/Pixoo_Album_Art/main.py $TRACK_ID $PLAYER_EVENT
+/usr/bin/PixelArt_x_Spotify/env/bin/source activate
+python3 /usr/bin/PixelArt_x_Spotify/main.py $TRACK_ID $PLAYER_EVENT
 fi
